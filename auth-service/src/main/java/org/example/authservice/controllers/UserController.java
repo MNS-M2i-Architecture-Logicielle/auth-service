5de31024371c020caf9956ea5dd527865e584041
thomas <tomceg@hotmail.fr>
2025-06-20 11:29:20 +0200

    modify constructor of controller

--- auth-service/src/main/java/org/example/authservice/controllers/UserController.java
+++ auth-service/src/main/java/org/example/authservice/controllers/UserController.java
@@ -15,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class UserController {
 
-        @Autowired
-        private UserService userService;
+        private final UserService userService;
+
+        public UserController(UserService userService){
+                this.userService = userService;
+        }
 
         @PostMapping("/register")
         public User register(@RequestBody RegisterRequest registerRequest) {
